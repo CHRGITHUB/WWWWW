be361c9fac6294f4ce4451cd37e2428825236d97
chenhongrong <chr1132026302@icloud.com>
2016-09-04 17:02:06 +0800

    添加文件

---
+++ WWWWW/WWWWW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWWWW
+//
+//  Created by 陈红荣 on 16/9/4.
+//  Copyright © 2016年 chenHongRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWWWW/WWWWW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWWWW
+//
+//  Created by 陈红荣 on 16/9/4.
+//  Copyright © 2016年 chenHongRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
